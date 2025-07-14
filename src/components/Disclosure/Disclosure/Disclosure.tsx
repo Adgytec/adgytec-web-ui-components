0b2d9853d3df0b448e6d2392a9c5da7d961c919f
rohan <rohanverma031@gmail.com>
2025-07-14 20:36:55 +0530

    fix: fixed disclosure build error

--- src/components/Disclosure/Disclosure/Disclosure.tsx
+++ src/components/Disclosure/Disclosure/Disclosure.tsx
@@ -9,44 +9,23 @@ import TextButton from "../../Button/TextButton";
 import { ColorTheme } from "../../../utils/types";
 import { ButtonShape } from "../../Button/types";
 
-const Disclosure = ({
-  heading,
-  content,
-  children,
-  ...props
-}: DisclosureProps) => {
-  if (heading && content) {
-    return (
-      <UnstyledDisclousre
-        {...props}
-        className={props.className ?? styles["disclosure"]}
-      >
-        <h2 className={styles["trigger"]}>
-          <TextButton
-            slot="trigger"
-            theme={ColorTheme.inverseSurface}
-            shape={ButtonShape.shrink}
-          >
-            <ChevronRight strokeWidth={3} />
-            {heading}
-          </TextButton>
-        </h2>
-
-        <DisclosurePanel className={styles["panel"]}>
-          <p>{content}</p>
-        </DisclosurePanel>
-      </UnstyledDisclousre>
-    );
-  }
-
-  const isChildFunc = typeof children === "function";
-
+const Disclosure = ({ heading, children, id }: DisclosureProps) => {
   return (
-    <UnstyledDisclousre
-      {...props}
-      className={props.className ?? styles["disclosure"]}
-    >
-      {isChildFunc ? (values) => children(values) : children}
+    <UnstyledDisclousre className={styles["disclosure"]} id={id}>
+      <h2 className={styles["trigger"]}>
+        <TextButton
+          slot="trigger"
+          theme={ColorTheme.inverseSurface}
+          shape={ButtonShape.shrink}
+        >
+          <ChevronRight strokeWidth={3} />
+          {heading}
+        </TextButton>
+      </h2>
+
+      <DisclosurePanel className={styles["panel"]}>
+        <p>{children}</p>
+      </DisclosurePanel>
     </UnstyledDisclousre>
   );
 };
