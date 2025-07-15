40eb4e321825dfebfa5a6c9265f801ae9d68669d
rohan <rohanverma031@gmail.com>
2025-07-15 13:37:52 +0530

    feat: added publishing config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,53 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vite/client" />
+import path, { resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { globSync } from "glob";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({
+      tsconfigPath: "tsconfig.app.json",
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      input: Object.fromEntries(
+        globSync(["src/components/**/index.ts", "src/index.ts"]).map((file) => {
+          // This remove `src/` as well as the file extension from each
+          // file, so e.g. src/nested/foo.js becomes nested/foo
+          const entryName = path.relative(
+            "src",
+            file.slice(0, file.length - path.extname(file).length),
+          );
+          // This expands the relative paths to absolute paths, so e.g.
+          // src/nested/foo becomes /project/src/nested/foo.js
+          const entryUrl = fileURLToPath(new URL(file, import.meta.url));
+          return [entryName, entryUrl];
+        }),
+      ),
+      external: [
+        "react",
+        "react-dom",
+        "react/jsx-runtime",
+        "react-aria-components",
+        "usehooks-ts",
+        "lucide-react",
+      ],
+      output: {
+        entryFileNames: "[name].js",
+        assetFileNames: "assets/[name][extname]",
+      },
+    },
+  },
+});
