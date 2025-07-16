c377cd20165cdb361555cb14623b89a7436e7d66
rohan <rohanverma031@gmail.com>
2025-07-16 15:42:14 +0530

    feat: added script to generate all the public component export in root index.ts

---
+++ src/scripts/generate-exports.js
@@ -0,0 +1,35 @@
+import { writeFileSync } from "fs";
+import { globSync } from "glob";
+import path from "path";
+
+// ⬇️ Manual exports can be declared here
+const manualExports = ['export * from "./utils/types";'];
+
+// ⬇️ Auto-collect component exports
+const files = globSync("src/components/**/index.ts");
+
+const componentExports = files.map((filePath) => {
+  let relativePath = filePath;
+  relativePath = relativePath.slice(0, -9);
+  relativePath = relativePath.slice(4);
+  relativePath = "./" + relativePath;
+  relativePath = relativePath.replaceAll("\\", "/");
+
+  return `export * from "${relativePath}";`;
+});
+
+// Sort exports alphabetically
+componentExports.sort((a, b) => a.localeCompare(b));
+
+// Final output
+const outputPath = "src/index.ts";
+const banner = `// AUTO-GENERATED FILE — DO NOT EDIT MANUALLY\n\n`;
+const finalContent =
+  banner + [...manualExports, "", ...componentExports].join("\n") + "\n";
+
+// Write to index.ts
+writeFileSync(outputPath, finalContent);
+
+console.log(
+  `✅ Generated ${outputPath} with ${componentExports.length} component exports.`,
+);
