3b01dd677a8d55ecc61b4acef2a45a8eae2feff3
rohan <rohanverma031@gmail.com>
2025-07-20 15:53:48 +0530

    fix: fixed submit handler return type

--- src/components/Form/Form/types.ts
+++ src/components/Form/Form/types.ts
@@ -5,16 +5,13 @@ export type FormReset = () => void;
 
 // this submit handler can optionally return field level errors
 // success is handled by parent component and form level error also
-export type submitHandler<T> = (
+export type SubmitHandler<T> = (
   values: T,
   reset: FormReset,
-) =>
-  | void
-  | Promise<void>
-  | Promise<Partial<Record<keyof T, string | string[]>>>;
+) => void | Promise<void | Partial<Record<keyof T, string | string[]>>>;
 
 export interface FormProps<T extends z.ZodObject<any>>
   extends Omit<AriaFormProps, "onSubmit" | "validationErrors"> {
   schema: T;
-  onSubmit: submitHandler<z.infer<T>>;
+  onSubmit: SubmitHandler<z.infer<T>>;
 }
