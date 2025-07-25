aa9b23303c458cdf49ba0d8187e0af86afd907cf
rohan <rohanverma031@gmail.com>
2025-07-25 19:04:25 +0530

    update: made PopoverDialogProps elements optional

--- src/components/Popover/types.ts
+++ src/components/Popover/types.ts
@@ -8,6 +8,6 @@ import type { ButtonVariantProps } from "../Button";
 
 export interface PopoverDialogProps extends DialogProps {
   trigger: ReactElement<ButtonVariantProps>;
-  popoverProps: Omit<AriaPopoverProps, "children">;
-  dialogTriggerProps: Omit<DialogTriggerProps, "children">;
+  popoverProps?: Omit<AriaPopoverProps, "children">;
+  dialogTriggerProps?: Omit<DialogTriggerProps, "children">;
 }
