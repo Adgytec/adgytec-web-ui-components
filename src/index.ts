0c6c2ca31f5087038f744e480b6fe7baa37b4d53
rohan <rohanverma031@gmail.com>
2025-08-05 12:55:10 +0530

    feat: added export script to add exports to package json

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,3 @@
 // AUTO-GENERATED FILE — DO NOT EDIT MANUALLY
 
 export * from "./utils/types";
-
-export * from "./components/Avatar";
-export * from "./components/Button/ButtonBase";
-export * from "./components/Button/ButtonChild";
-export * from "./components/Button/FilledButton";
-export * from "./components/Button/OutlinedButton";
-export * from "./components/Button/TextButton";
-export * from "./components/Button/ToggleButton";
-export * from "./components/Disclosure/Disclosure";
-export * from "./components/Disclosure/DisclousureGroup";
-export * from "./components/Error";
-export * from "./components/Form/Form";
-export * from "./components/Form/Input";
-export * from "./components/Form/TextArea";
-export * from "./components/Link/FilledButtonLink";
-export * from "./components/Link/LinkBase";
-export * from "./components/Link/OutlinedButtonLink";
-export * from "./components/Loader";
-export * from "./components/Menu/MenuBase";
-export * from "./components/Menu/MenuButton";
-export * from "./components/Menu/MenuLabel";
-export * from "./components/Modal/ModalAction";
-export * from "./components/Modal/ModalBase";
-export * from "./components/Navigation/NavigationMenu";
-export * from "./components/Navigation/NavigationResponsive";
-export * from "./components/Navigation/NavigationSidebar";
-export * from "./components/Popover/PopoverBase";
-export * from "./components/Popover/PopoverDialog";
-export * from "./components/Select";
-export * from "./components/Sidebar";
-export * from "./components/Success";
-export * from "./components/ToggleButtonGroup";
-export * from "./components/Tooltip";
-export * from "./components/Tree";
--- src/scripts/generate-exports.js
+++ src/scripts/generate-exports.js
@@ -5,30 +5,34 @@ import { globSync } from "glob";
 const manualExports = ['export * from "./utils/types";'];
 
 // ⬇️ Auto-collect component exports
-const files = globSync("src/components/**/index.ts");
-
-const componentExports = files.map((filePath) => {
-  let relativePath = filePath;
-  relativePath = relativePath.slice(0, -9);
-  relativePath = relativePath.slice(4);
-  relativePath = "./" + relativePath;
-  relativePath = relativePath.replaceAll("\\", "/");
-
-  return `export * from "${relativePath}";`;
-});
-
+// const files = globSync("src/components/**/index.ts");
+//
+// const componentExports = files.map((filePath) => {
+//   let relativePath = filePath;
+//   relativePath = relativePath.slice(0, -9);
+//   relativePath = relativePath.slice(4);
+//   relativePath = "./" + relativePath;
+//   relativePath = relativePath.replaceAll("\\", "/");
+//
+//   return `export * from "${relativePath}";`;
+// });
+//
 // Sort exports alphabetically
-componentExports.sort((a, b) => a.localeCompare(b));
+// componentExports.sort((a, b) => a.localeCompare(b));
 
 // Final output
 const outputPath = "src/index.ts";
 const banner = `// AUTO-GENERATED FILE — DO NOT EDIT MANUALLY\n\n`;
-const finalContent =
-  banner + [...manualExports, "", ...componentExports].join("\n") + "\n";
+// const finalContent =
+//   banner + [...manualExports, "", ...componentExports].join("\n") + "\n";
+//
+const finalContent = banner + [...manualExports].join("\n") + "\n";
 
 // Write to index.ts
 writeFileSync(outputPath, finalContent);
 
-console.log(
-  `✅ Generated ${outputPath} with ${componentExports.length} component exports.`,
-);
+// console.log(
+//   `✅ Generated ${outputPath} with ${componentExports.length} component exports.`,
+// );
+
+console.log(`✅ Generated ${outputPath} component exports.`);
