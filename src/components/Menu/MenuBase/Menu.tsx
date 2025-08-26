a203a0333eca537b04a352b360445e5d416eac66
rohan <rohanverma031@gmail.com>
2025-08-26 15:23:06 +0530

    feat: added card component to menu component for styles

--- src/components/Menu/MenuBase/Menu.tsx
+++ src/components/Menu/MenuBase/Menu.tsx
@@ -9,8 +9,9 @@ import {
 import styles from "./menu.module.css";
 import { Popover } from "@/components/Popover/PopoverBase";
 import { ChevronRight } from "lucide-react";
+import { BaseCard, CardBackground } from "@/components/Card/BaseCard";
 
-const RenderMenu = ({ menuItem }: RenderMenuProps) => {
+const RenderMenu = ({ menuItem, cardBackground }: RenderMenuProps) => {
   if (menuItem.type === "separator") {
     return <Separator className={`${styles["separator"]}`} />;
   }
@@ -35,26 +36,46 @@ const RenderMenu = ({ menuItem }: RenderMenuProps) => {
     <SubmenuTrigger>
       {menuComp}
       <Popover>
-        <UnstyledMenu className={`${styles["menu"]}`}>
-          {menuItem.subItems!.map((subMenu) => {
-            return <RenderMenu key={subMenu.id} menuItem={subMenu} />;
-          })}
-        </UnstyledMenu>
+        <BaseCard background={cardBackground}>
+          <UnstyledMenu className={`${styles["menu"]}`}>
+            {menuItem.subItems!.map((subMenu) => {
+              return (
+                <RenderMenu
+                  key={subMenu.id}
+                  menuItem={subMenu}
+                  cardBackground={cardBackground}
+                />
+              );
+            })}
+          </UnstyledMenu>
+        </BaseCard>
       </Popover>
     </SubmenuTrigger>
   );
 };
 
-export const Menu = ({ children, menuItems }: MenuProps) => {
+export const Menu = ({
+  children,
+  menuItems,
+  cardBackground = CardBackground.gradient,
+}: MenuProps) => {
   return (
     <MenuTrigger>
       {children}
       <Popover>
-        <UnstyledMenu className={`${styles["menu"]}`}>
-          {menuItems.map((item) => {
-            return <RenderMenu key={item.id} menuItem={item} />;
-          })}
-        </UnstyledMenu>
+        <BaseCard background={cardBackground}>
+          <UnstyledMenu className={`${styles["menu"]}`}>
+            {menuItems.map((item) => {
+              return (
+                <RenderMenu
+                  key={item.id}
+                  cardBackground={cardBackground}
+                  menuItem={item}
+                />
+              );
+            })}
+          </UnstyledMenu>
+        </BaseCard>
       </Popover>
     </MenuTrigger>
   );
--- src/components/Menu/MenuBase/types.ts
+++ src/components/Menu/MenuBase/types.ts
@@ -1,10 +1,12 @@
 import type { ReactElement, ReactNode } from "react";
 import type { HierarchyItemType } from "@/utils/types";
 import type { ButtonVariantProps } from "@/components/Button/ButtonBase/types";
+import type { CardBackground } from "@/components/Card/BaseCard";
 
 export interface MenuProps {
   children: ReactNode;
   menuItems: HierarchyItemType[];
+  cardBackground?: CardBackground;
 }
 
 export interface MenuButtonProps {
@@ -20,4 +22,5 @@ export interface MenuLabelProps {
 
 export interface RenderMenuProps {
   menuItem: HierarchyItemType;
+  cardBackground: CardBackground;
 }
