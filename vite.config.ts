b86f5fa0ede5e1fb0c5b2c2612b9edd2e81b6671
rohan <rohanverma031@gmail.com>
2026-01-15 16:11:31 +0530

    feat: added new vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,45 @@
+import { resolve } from "node:url";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
 
-export default defineConfig({});
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: "tsconfig.app.json",
+      insertTypesEntry: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+      fileName: "index",
+    },
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        dir: "dist",
+        preserveModules: true,
+        preserveModulesRoot: "src",
+        entryFileNames: "[name]-[format]-[hash:10].js",
+        assetFileNames: "assets/[name]-[hash:10].[ext]",
+      },
+      external: [
+        "react",
+        "react-dom",
+        "react-aria-components",
+        "lucide-react",
+        "usehooks-ts",
+        "zod",
+      ],
+    },
+  },
+});
