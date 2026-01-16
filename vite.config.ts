ceab38f684ec25180c74d9a9174fc22e9526342e
rohan <rohanverma031@gmail.com>
2026-01-16 16:37:53 +0530

    feat: added styles exports

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { resolve } from "node:path";
+import path, { resolve } from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
 import dts from "vite-plugin-dts";
 import optimizeLocales from "@react-aria/optimize-locales-plugin";
+import { fileURLToPath } from "node:url";
+import { globSync } from "glob";
 
 export default defineConfig({
   plugins: [
@@ -27,6 +29,7 @@ export default defineConfig({
   },
   build: {
     outDir: "dist",
+    copyPublicDir: false,
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       formats: ["es"],
@@ -34,17 +37,33 @@ export default defineConfig({
     },
     cssCodeSplit: true,
     rollupOptions: {
-      input: {
-        index: "src/index.ts",
-        "styles/main": "src/styles/index-main.ts",
-        "styles/reset": "src/styles/index-reset.ts",
-      },
+      input: Object.fromEntries([
+        // 1️⃣ Auto-discovered entries (components + root index)
+        ...globSync(["src/components/**/index.ts", "src/index.ts"]).map(
+          (file) => [
+            // entry name: remove `src/` and extension
+            path.relative(
+              "src",
+              file.slice(0, file.length - path.extname(file).length),
+            ),
+            // absolute file path
+            fileURLToPath(new URL(file, import.meta.url)),
+          ],
+        ),
+
+        // 2️⃣ Explicit style entries
+        [
+          "styles/main",
+          fileURLToPath(new URL("src/styles/main.ts", import.meta.url)),
+        ],
+        [
+          "styles/reset",
+          fileURLToPath(new URL("src/styles/reset.ts", import.meta.url)),
+        ],
+      ]),
       output: {
-        dir: "dist",
-        preserveModules: true,
-        preserveModulesRoot: "src",
         entryFileNames: "[name].js",
-        assetFileNames: "[name].[ext]",
+        assetFileNames: "assets/[name].[ext]",
       },
       external: [
         "react",
