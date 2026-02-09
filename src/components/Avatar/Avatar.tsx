a7f7606812dd300feb5737fe5cf3d4ab92245802
rohan <rohanverma031@gmail.com>
2026-02-09 12:18:19 +0530

    refactor: refactored avatar comp to use new image comp

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,34 +1,36 @@
 import type { AvatarBaseProps, AvatarProps } from "./types";
 import styles from "./avatar.module.css";
+import { Image } from "@/components/Image";
 
 const AvatarBase = ({ children, size, type, theme }: AvatarBaseProps) => {
-  return (
-    <div
-      className={`${styles["avatar"]} ${styles[size]} ${styles[type]} ${styles[theme]}`}
-    >
-      {children}
-    </div>
-  );
+    return (
+        <div
+            className={`${styles["avatar"]} ${styles[size]} ${styles[type]} ${styles[theme]}`}
+        >
+            {children}
+        </div>
+    );
 };
 
 export const Avatar = ({
-  src,
-  children,
-  size = "normal",
-  label,
-  theme = "primary",
+    type,
+    image,
+    children,
+    size = "normal",
+    label,
+    theme = "primary",
 }: AvatarProps) => {
-  if (children) {
-    return (
-      <AvatarBase type="node" size={size} theme={theme}>
-        {children}
-      </AvatarBase>
-    );
-  }
+    if (type === "children") {
+        return (
+            <AvatarBase type="node" size={size} theme={theme}>
+                {children}
+            </AvatarBase>
+        );
+    }
 
-  return (
-    <AvatarBase size={size} type="image" theme={theme}>
-      <img src={src} alt={label} />
-    </AvatarBase>
-  );
+    return (
+        <AvatarBase size={size} type="image" theme={theme}>
+            <Image {...image} alt={label} />
+        </AvatarBase>
+    );
 };
