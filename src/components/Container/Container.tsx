6f2345e20e2972dbe7a950e40bde46100fd4652c
rohan <rohanverma031@gmail.com>
2026-02-10 20:12:28 +0530

    feat: added container impl

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,10 @@
+import type { ContainerProps } from "./types";
+import styles from "./container.module.css";
+
+export const Container: React.FC<ContainerProps> = ({
+    varaint = "default",
+    className,
+    ...props
+}) => {
+    return <div className={`${styles[varaint]} ${className}`} {...props} />;
+};
