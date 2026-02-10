f7f95115c79d1e1441309e3ac33cd48c3938a7e5
rohan <rohanverma031@gmail.com>
2026-02-10 19:33:57 +0530

    update: added vw value on initial render

--- src/components/Viewport/Viewport.tsx
+++ src/components/Viewport/Viewport.tsx
@@ -2,11 +2,15 @@ import { useEffect } from "react";
 
 export const Viewport = () => {
     useEffect(() => {
-        // removes classic scroll bar problem
-        const observer = new ResizeObserver(() => {
-            let vw = document.documentElement.clientWidth / 100;
+        const updateVw = () => {
+            const vw = document.documentElement.clientWidth / 100;
             document.documentElement.style.setProperty("--vw", `${vw}px`);
-        });
+        };
+
+        updateVw(); // Set the value on initial render
+
+        // removes classic scroll bar problem
+        const observer = new ResizeObserver(updateVw);
         observer.observe(document.documentElement);
 
         return () => {
