17c6943ada6b109518dcbdc4b5685a760319b54b
rohan <rohanverma031@gmail.com>
2026-02-11 13:13:28 +0530

    update: removed button-link class from base button

--- src/components/Button/ButtonBase/Button.tsx
+++ src/components/Button/ButtonBase/Button.tsx
@@ -28,7 +28,6 @@ export const Button = ({
                 {...props}
                 className={clsx(
                     styles["button"],
-                    styles["button-link"],
                     styles[variant],
                     styles[theme],
                     styles[shape],
