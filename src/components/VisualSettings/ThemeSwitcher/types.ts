672762edf102fb2c652c2acf1f14fa48c8af26a2
rohan <rohanverma031@gmail.com>
2026-03-01 11:04:51 +0530

    refactor: refactored theme switcher to get values from caller

--- src/components/VisualSettings/ThemeSwitcher/types.ts
+++ src/components/VisualSettings/ThemeSwitcher/types.ts
@@ -1,6 +1,13 @@
 import type { ColorTheme } from "@/utils/types";
 
+export type ThemeValue = {
+    system: string;
+    light: string;
+    dark: string;
+};
+
 export interface ThemeSwitcherProps {
-  ui?: boolean;
-  theme?: ColorTheme;
+    ui?: boolean;
+    theme?: ColorTheme;
+    displayValue?: ThemeValue;
 }
