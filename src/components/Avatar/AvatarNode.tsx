1836cda2fee37050cbf11ad80ac43d8d5c53084d
rohan <rohanverma031@gmail.com>
2026-04-05 16:26:55 +0530

    feat: added avatar node impl

--- src/components/Avatar/AvatarNode.tsx
+++ src/components/Avatar/AvatarNode.tsx
@@ -1,3 +1,7 @@
-export const AvatarNode = () => {
-    return <div></div>;
+import clsx from "clsx";
+import styles from "./avatar.module.css";
+import type { AvatarNodeProps } from "./types";
+
+export const AvatarNode: React.FC<AvatarNodeProps> = ({ children }) => {
+    return <div className={clsx(styles["node"])}>{children}</div>;
 };
