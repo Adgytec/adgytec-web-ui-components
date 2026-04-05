7f9e3cd5ad8dbeca984337a46f39c81316bbffa4
rohan <rohanverma031@gmail.com>
2026-04-05 16:28:44 +0530

    feat: updated avatar impl

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,53 +1,15 @@
-// TODO: better api for clients
-// remove type parameter
-// client directly send nullable image that will be passed to Image components
-// and required string whose first letter will be displayed if no image details
-// will also use that string for image alt value
-
 import clsx from "clsx";
-import { Image } from "@/components/Image";
 import styles from "./avatar.module.css";
-import type { AvatarBaseProps, AvatarProps, AvatarSize } from "./types";
+import type { AvatarProps } from "./types";
 
-const AVATAR_RENDER_SIZE: Record<AvatarSize, number> = {
-    small: 32,
-    normal: 48,
-    large: 64,
-};
-
-const AvatarBase = ({ children, size, type, theme }: AvatarBaseProps) => {
-    return (
-        <div
-            className={clsx(
-                styles.avatar,
-                styles[size],
-                styles[type],
-                styles[theme]
-            )}
-        >
-            {children}
-        </div>
-    );
-};
-
-export const Avatar = ({
-    type,
-    image,
+export const Avatar: React.FC<AvatarProps> = ({
     children,
     size = "normal",
     theme = "primary",
-}: AvatarProps) => {
-    if (type === "children") {
-        return (
-            <AvatarBase type="node" size={size} theme={theme}>
-                {children}
-            </AvatarBase>
-        );
-    }
-
+}) => {
     return (
-        <AvatarBase size={size} type="image" theme={theme}>
-            <Image {...image} sizes={`${AVATAR_RENDER_SIZE[size]}px`} />
-        </AvatarBase>
+        <div className={clsx(styles.avatar, styles[size], theme)}>
+            {children}
+        </div>
     );
 };
