892d54a95a4214c65094b359ccbf2a5576632fde
rohan <rohanverma031@gmail.com>
2026-04-05 16:23:48 +0530

    feat: added avatar image implementation

--- src/components/Avatar/AvatarImage.tsx
+++ src/components/Avatar/AvatarImage.tsx
@@ -1,3 +1,20 @@
-export const AvatarImage = () => {
-    return <div></div>;
+import { clsx } from "clsx";
+import { Image } from "../Image";
+import styles from "./avatar.module.css";
+import type { AvatarImageProps } from "./types";
+
+export const AvatarImage: React.FC<AvatarImageProps> = ({ image, alt }) => {
+    // max avatar render size is 3rem * 3rem
+    return (
+        <Image
+            className={clsx(styles["image"])}
+            alt={alt}
+            preset="thumbnail"
+            thumbnail={image}
+            small={image}
+            medium={image}
+            large={image}
+            extraLarge={image}
+        />
+    );
 };
