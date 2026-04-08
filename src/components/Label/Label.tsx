cc76161032bca48062a2fb7e6a956ec6d55887bd
rohan <rohanverma031@gmail.com>
2026-04-08 09:29:39 +0530

    feat: exported label component

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -1,16 +1,12 @@
-// TODO: will be exported
-
 import clsx from "clsx";
 import { type LabelProps, Label as UnstyledLabel } from "react-aria-components";
 import styles from "./label.module.css";
 
-export const Label = (props: LabelProps) => {
+export const Label: React.FC<LabelProps> = ({ className, ...props }) => {
     return (
         <UnstyledLabel
+            className={clsx(styles["label"], className)}
             {...props}
-            className={clsx(styles["label"], props?.className)}
-        >
-            {props.children}
-        </UnstyledLabel>
+        />
     );
 };
