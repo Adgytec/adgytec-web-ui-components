8142b9a45974a0d5e52b27fdcc71f3d47cbf8532
rohan <rohanverma031@gmail.com>
2026-04-08 12:57:08 +0530

    feat: Added text area component

---
+++ src/components/TextArea/TextArea.tsx
@@ -0,0 +1,37 @@
+import clsx from "clsx";
+import { TextArea as AriaTextArea, TextField } from "react-aria-components";
+import styles from "@/utils/textField/textField.module.css";
+import type { TextFieldProps } from "@/utils/textField/types";
+import { Description } from "../Description";
+import { FieldError } from "../FieldError";
+import { Label } from "../Label";
+
+export const TextArea: React.FC<TextFieldProps> = ({
+    label,
+    description,
+    placeholder,
+    className,
+    ...props
+}) => {
+    return (
+        <TextField
+            className={(renderProps) =>
+                clsx(
+                    styles["text-field"],
+                    typeof className === "function"
+                        ? className(renderProps)
+                        : className
+                )
+            }
+            {...props}
+        >
+            {label && <Label>{label}</Label>}
+            <AriaTextArea
+                placeholder={placeholder}
+                className={clsx(styles["editor"], styles["textarea"])}
+            />
+            {description && <Description>{description}</Description>}
+            <FieldError />
+        </TextField>
+    );
+};
