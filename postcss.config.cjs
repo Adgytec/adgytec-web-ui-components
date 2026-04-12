80bdcf5c2e9496440d90f0f7f3b4620285c23dae
rohan <rohanverma031@gmail.com>
2026-04-12 12:44:36 +0530

    update: updated post css config and added color-scheme value

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,7 +3,7 @@ const postcssGlobalData = require("@csstools/postcss-global-data");
 module.exports = {
     plugins: [
         postcssGlobalData({
-            files: ["./src/styles/core/layout.css"],
+            files: ["./src/styles/core/tokens/layout.css"],
         }),
         require("postcss-nesting"),
         require("postcss-custom-media")({
