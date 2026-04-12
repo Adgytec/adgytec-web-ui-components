e9e697481ab63201791f463d44625359fbaa2b72
rohan <rohanverma031@gmail.com>
2026-04-12 16:27:23 +0530

    feat: updated icon to handle with-text according to material spec

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,23 +1,33 @@
+// material symbols with typography ref
+// https://m3.material.io/styles/icons/applying-icons#f9db4adc-ca78-473f-85eb-a351b73c39ac
+
 import clsx from "clsx";
 import styles from "./icon.module.css";
 import type { IconProps } from "./types";
 
 export const Icon: React.FC<IconProps> = ({
-    size = "medium",
+    size,
     weight,
-    fill = false,
-    withText = false,
+    fill,
+    withText,
     icon,
 }) => {
+    if (process.env.NODE_ENV !== "production") {
+        if (withText && (size || weight)) {
+            console.warn(
+                "Icon: 'size' and 'weight' are ignored when 'withText' is true."
+            );
+        }
+    }
+
     return (
         <span
             className={clsx(
                 "material-symbols-outlined",
-                styles["icon"],
-                styles[size],
+                withText ? styles["with-text"] : styles["icon"],
+                size && styles[size],
                 fill && styles["fill"],
-                weight && styles[weight],
-                withText && styles["with-text"]
+                weight && styles[`weight-${weight}`]
             )}
         >
             {icon}
--- src/components/Icon/types.ts
+++ src/components/Icon/types.ts
@@ -5,12 +5,12 @@ export type IconSize =
     | "large"
     | "extra-large";
 
-export type IconWeight = "regular" | "w-medium" | "bold";
+export type IconWeight = "regular" | "medium" | "bold";
 
 export type IconProps = {
+    icon: string;
     size?: IconSize;
     weight?: IconWeight;
     fill?: boolean;
     withText?: boolean;
-    icon: string;
 };
