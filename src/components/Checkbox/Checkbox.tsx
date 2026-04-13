59222e41af894c0c852f9b3bd6165fb908471cfe
rohan <rohanverma031@gmail.com>
2026-04-13 16:02:57 +0530

    feat: added basic checkbox component

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,3 +1,55 @@
-export const Checkbox = () => {
-    return <div></div>;
+import clsx from "clsx";
+import { Checkbox as AriaCheckbox } from "react-aria-components";
+import { Target } from "../Target";
+import styles from "./checkbox.module.css";
+import type { CheckboxProps } from "./types";
+
+export const Checkbox: React.FC<CheckboxProps> = ({
+    children,
+    className,
+    ...props
+}) => {
+    return (
+        <AriaCheckbox
+            className={(renderProps) =>
+                clsx(
+                    styles["checkbox"],
+                    typeof className === "function"
+                        ? className(renderProps)
+                        : className
+                )
+            }
+            {...props}
+        >
+            {({
+                isSelected,
+                isHovered,
+                isDisabled,
+                isFocused,
+                isFocusVisible,
+                isPressed,
+            }) => {
+                const dataAttrs = {
+                    "data-selected": isSelected || undefined,
+                    "data-hovered": isHovered || undefined,
+                    "data-disabled": isDisabled || undefined,
+                    "data-focused": isFocused || undefined,
+                    "data-focus-visible": isFocusVisible || undefined,
+                    "data-pressed": isPressed || undefined,
+                };
+
+                return (
+                    <>
+                        <Target>
+                            <div
+                                className={styles["indicator"]}
+                                {...dataAttrs}
+                            ></div>
+                        </Target>
+                        {children}
+                    </>
+                );
+            }}
+        </AriaCheckbox>
+    );
 };
--- src/components/Checkbox/types.tsx
+++ src/components/Checkbox/types.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 import type {
     CheckboxGroupProps as AraiaCheckboxGroupProps,
+    CheckboxProps as AriaCheckboxProps,
     ValidationResult,
 } from "react-aria-components";
 
@@ -9,5 +10,9 @@ export interface CheckboxGroupProps
     label?: string;
     description?: string;
     errorMessage?: string | ((validation: ValidationResult) => string);
+    children?: ReactNode;
+}
+
+export interface CheckboxProps extends Omit<AriaCheckboxProps, "children"> {
     children?: ReactNode;
 }
