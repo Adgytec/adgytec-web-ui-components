f1c81fcbaff5ebd1c620ac299a5a964dfffd73b5
rohan <rohanverma031@gmail.com>
2026-04-14 16:56:47 +0530

    feat: added tooltip base comp

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,3 +1,22 @@
-export const Tooltip = () => {
-    return <div></div>;
+import clsx from "clsx";
+import {
+    Tooltip as AriaTooltip,
+    type TooltipProps,
+} from "react-aria-components";
+import styles from "./tooltip.module.css";
+
+export const Tooltip: React.FC<TooltipProps> = ({ className, ...props }) => {
+    return (
+        <AriaTooltip
+            className={(renderProps) =>
+                clsx(
+                    styles["tooltip"],
+                    typeof className === "function"
+                        ? className(renderProps)
+                        : className
+                )
+            }
+            {...props}
+        />
+    );
 };
