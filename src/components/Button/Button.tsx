1edf79d3f696c9e2861707026ac103fd65dbd143
rohan <rohanverma031@gmail.com>
2026-04-14 17:24:23 +0530

    update: added custom Tooltip trigger

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,9 @@
+// TODO: impl will be updated
+
 import { clsx } from "clsx";
 import { Button as UnstyledButton } from "react-aria-components";
 import { Splash } from "@/components/Splash/Splash.tsx";
 import { useSplash } from "@/components/Splash/useSplash.ts";
-import { Tooltip } from "@/components/Tooltip";
 import styles from "@/utils/button/button.module.css";
 import type { ButtonProps } from "./types.ts";
 
@@ -20,36 +21,34 @@ export const Button: React.FC<ButtonProps> = ({
     const isChildFunc = typeof children === "function";
 
     return (
-        <Tooltip theme={theme} description={description}>
-            <UnstyledButton
-                className={(renderProps) =>
-                    clsx(
-                        styles["button"],
-                        styles[variant],
-                        theme,
-                        styles[shape],
-                        typeof className === "function"
-                            ? className(renderProps)
-                            : className
-                    )
-                }
-                onPress={handlePress}
-                {...props}
-            >
-                {isChildFunc ? (
-                    (buttonState) => (
-                        <>
-                            {coords && <Splash {...coords} />}
-                            {children(buttonState)}
-                        </>
-                    )
-                ) : (
+        <UnstyledButton
+            className={(renderProps) =>
+                clsx(
+                    styles["button"],
+                    styles[variant],
+                    theme,
+                    styles[shape],
+                    typeof className === "function"
+                        ? className(renderProps)
+                        : className
+                )
+            }
+            onPress={handlePress}
+            {...props}
+        >
+            {isChildFunc ? (
+                (buttonState) => (
                     <>
                         {coords && <Splash {...coords} />}
-                        {children}
+                        {children(buttonState)}
                     </>
-                )}
-            </UnstyledButton>
-        </Tooltip>
+                )
+            ) : (
+                <>
+                    {coords && <Splash {...coords} />}
+                    {children}
+                </>
+            )}
+        </UnstyledButton>
     );
 };
---
+++ src/components/Tooltip/TooltipTrigger.tsx
@@ -0,0 +1,12 @@
+import {
+    TooltipTrigger as AriaTooltipTrigger,
+    type TooltipTriggerComponentProps,
+} from "react-aria-components";
+
+export const TooltipTrigger: React.FC<TooltipTriggerComponentProps> = ({
+    delay = 0,
+    closeDelay = 0,
+    ...props
+}) => {
+    return <AriaTooltipTrigger {...props} />;
+};
