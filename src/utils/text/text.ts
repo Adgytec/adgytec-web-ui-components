85c9ab9b97f08a1da13f404be50f6c21f1fd5952
rohan <rohanverma031@gmail.com>
2026-04-14 13:26:14 +0530

    feat: added text classes with predefined css styles

--- src/utils/text/text.ts
+++ src/utils/text/text.ts
@@ -1,3 +1,23 @@
-export const text = {} as const;
+export const typography = {
+    displayLarge: "typography-display-large",
+    displayMedium: "typography-display-medium",
+    displaySmall: "typography-display-small",
 
-export type TextVariant = keyof typeof text;
+    headlineLarge: "typography-headline-large",
+    headlineMedium: "typography-headline-medium",
+    headlineSmall: "typography-headline-small",
+
+    titleLarge: "typography-title-large",
+    titleMedium: "typography-title-medium",
+    titleSmall: "typography-title-small",
+
+    bodyLarge: "typography-body-large",
+    bodyMedium: "typography-body-medium",
+    bodySmall: "typography-body-small",
+
+    labelLarge: "typography-label-large",
+    labelMedium: "typography-label-medium",
+    labelSmall: "typography-label-small",
+} as const;
+
+export type TypographyVariant = keyof typeof typography;
