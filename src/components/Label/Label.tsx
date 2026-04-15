2eb2bedbb5d6446b6f0bc063fecedeea91e5a288
rohan <rohanverma031@gmail.com>
2026-04-15 13:34:14 +0530

    feat: updated label styles

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -1,9 +1,13 @@
 import clsx from "clsx";
 import { Label as AriaLabel, type LabelProps } from "react-aria-components";
 import styles from "./label.module.css";
+import { typography } from "@/utils/typography";
 
 export const Label: React.FC<LabelProps> = ({ className, ...props }) => {
     return (
-        <AriaLabel className={clsx(styles["label"], className)} {...props} />
+        <AriaLabel
+            className={clsx(styles["label"], typography.labelLarge, className)}
+            {...props}
+        />
     );
 };
