96aba830bca9cd4c29e4ab71d5695931908c0a97
rohan <rohanverma031@gmail.com>
2026-04-16 16:27:27 +0530

    feat: added helpers to get color configs

--- src/components/Button/core/color.ts
+++ src/components/Button/core/color.ts
@@ -1,3 +1,5 @@
+import styles from "./color.module.css";
+
 type CoreButtonColor = "filled" | "tonal" | "outlined";
 
 export type ButtonColor = CoreButtonColor | "elevated" | "text";
@@ -5,3 +7,9 @@ export type ButtonColor = CoreButtonColor | "elevated" | "text";
 export type ToggleButtonColor = CoreButtonColor | "elevated";
 
 export type IconButtonColor = CoreButtonColor | "standard";
+
+export const buttonColorConfig = (color: ButtonColor | IconButtonColor) => {
+    return styles[color];
+};
+
+export const buttonColorBase = styles["base"];
