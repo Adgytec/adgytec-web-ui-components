4765c453d66435b84eb87d34f0de0b975860a406
rohan <rohanverma031@gmail.com>
2026-04-16 13:59:45 +0530

    feat:added extra-small and small button sizes config

--- src/components/Button/core/sizes.ts
+++ src/components/Button/core/sizes.ts
@@ -1,6 +1,23 @@
+import { typography } from "@/utils/typography";
+
 export type ButtonSize =
     | "extra-small"
     | "small"
     | "medium"
     | "large"
     | "extra-large";
+
+export const IconSizeMapping: Record<ButtonSize, number> = {
+    "extra-small": 20,
+    small: 20,
+} as const;
+
+export const IconButtonSizeMapping: Record<ButtonSize, number> = {
+    "extra-small": 20,
+    small: 24,
+} as const;
+
+export const LabelTextMapping: Record<ButtonSize, string> = {
+    "extra-small": typography.labelLarge,
+    small: typography.labelLarge,
+} as const;
