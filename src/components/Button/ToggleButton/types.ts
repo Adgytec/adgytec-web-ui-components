adc03c99c748b3640392f9bccd4c145085c47e32
rohan <rohanverma031@gmail.com>
2026-04-16 16:15:45 +0530

    feat: added text button config

--- src/components/Button/ToggleButton/types.ts
+++ src/components/Button/ToggleButton/types.ts
@@ -1,6 +1,8 @@
 import type { ToggleButtonProps as AriaToggleButtonProps } from "react-aria-components";
-import type { ButtonBaseProps } from "../core";
+import type { ButtonBaseProps, ToggleButtonColor } from "../core";
 
 export interface ToggleButtonProps
     extends AriaToggleButtonProps,
-        ButtonBaseProps {}
+        Omit<ButtonBaseProps, "color"> {
+    color?: ToggleButtonColor;
+}
--- src/components/Button/core/color.ts
+++ src/components/Button/core/color.ts
@@ -2,4 +2,6 @@ type CoreButtonColor = "filled" | "tonal" | "outlined";
 
 export type ButtonColor = CoreButtonColor | "elevated" | "text";
 
+export type ToggleButtonColor = CoreButtonColor | "elevated";
+
 export type IconButtonColor = CoreButtonColor | "standard";
