34c33c24c68ba97629b66c9d32d3d0e93bd99e54
rohan <rohanverma031@gmail.com>
2026-04-16 14:15:05 +0530

    feat: added button icon and label size mapping

--- src/components/Button/core/sizes.ts
+++ src/components/Button/core/sizes.ts
@@ -7,17 +7,26 @@ export type ButtonSize =
     | "large"
     | "extra-large";
 
-export const IconSizeMapping: Record<ButtonSize, number> = {
+export const ButtonIconSizeMapping: Record<ButtonSize, number> = {
     "extra-small": 20,
     small: 20,
+    medium: 24,
+    large: 32,
+    "extra-large": 40,
 } as const;
 
-export const IconButtonSizeMapping: Record<ButtonSize, number> = {
+export const IconButtonIconSizeMapping: Record<ButtonSize, number> = {
     "extra-small": 20,
     small: 24,
+    medium: 24,
+    large: 32,
+    "extra-large": 40,
 } as const;
 
-export const LabelTextMapping: Record<ButtonSize, string> = {
+export const ButtonLabelTextMapping: Record<ButtonSize, string> = {
     "extra-small": typography.labelLarge,
     small: typography.labelLarge,
+    medium: typography.bodyLargeEmphasized,
+    large: typography.headlineSmall,
+    "extra-large": typography.headlineLarge,
 } as const;
