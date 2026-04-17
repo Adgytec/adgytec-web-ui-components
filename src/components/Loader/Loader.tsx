44b18841b661f567b9536dd13810522fe49dd850
rohan <rohanverma031@gmail.com>
2026-04-17 10:38:19 +0530

    feat: updated loader to use icon component

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,7 +1,16 @@
 import { clsx } from "clsx";
 import { LoaderCircle } from "lucide-react";
+import { Icon, type IconProps } from "../Icon";
 import styles from "./loader.module.css";
 
-export const Loader = () => {
-    return <LoaderCircle className={clsx(styles["loader"])} />;
+export const Loader: React.FC<Omit<IconProps, "icon" | "className">> = (
+    props
+) => {
+    return (
+        <Icon
+            icon={LoaderCircle}
+            className={clsx(styles["loader"])}
+            {...props}
+        />
+    );
 };
