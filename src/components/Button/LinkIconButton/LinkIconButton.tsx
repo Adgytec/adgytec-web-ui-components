263b750133426c34a3502f859c18fddd2c15448f
rohan <rohanverma031@gmail.com>
2026-04-17 15:26:33 +0530

    feat: added link icon button impl

--- src/components/Button/LinkIconButton/LinkIconButton.tsx
+++ src/components/Button/LinkIconButton/LinkIconButton.tsx
@@ -1,3 +1,73 @@
-export const LinkIconButton = () => {
-    return <div></div>;
+import { clsx } from "clsx";
+import { Link } from "react-aria-components";
+import { Icon } from "@/components/Icon";
+import { Loader } from "@/components/Loader";
+import { Splash } from "@/components/Splash/Splash";
+import { useSplash } from "@/components/Splash/useSplash";
+import { Target } from "@/components/Target";
+import {
+    ButtonCore,
+    ButtonReset,
+    ButtonSizeBase,
+    buttonColorBase,
+    buttonColorConfig,
+    buttonSizeConfig,
+    IconButtonIconSizeMapping,
+    withTooltip,
+} from "../core";
+import type { LinkIconButtonProps } from "./types";
+
+export const LinkIconButton: React.FC<LinkIconButtonProps> = ({
+    size = "small",
+    shape = "round",
+    color = "filled",
+    width = "default",
+    tooltip,
+    icon,
+    onPress,
+    ...props
+}) => {
+    const { splashInfo, handlePress } = useSplash(onPress);
+
+    return withTooltip(
+        <Link onPress={handlePress} className={clsx(ButtonReset)} {...props}>
+            {({
+                isDisabled,
+                isFocusVisible,
+                isFocused,
+                isPressed,
+                isHovered,
+            }) => {
+                const dataAttrs = {
+                    "data-hovered": isHovered || undefined,
+                    "data-disabled": isDisabled || undefined,
+                    "data-focused": isFocused || undefined,
+                    "data-focus-visible": isFocusVisible || undefined,
+                    "data-pressed": isPressed || undefined,
+                    "data-shape": shape,
+                    "data-width": width,
+                };
+
+                const iconSize = IconButtonIconSizeMapping[size];
+                return (
+                    <Target {...dataAttrs}>
+                        <div
+                            className={clsx(
+                                ButtonCore,
+                                buttonColorBase,
+                                ButtonSizeBase,
+                                buttonColorConfig(color),
+                                buttonSizeConfig(size)
+                            )}
+                            {...dataAttrs}
+                        >
+                            {splashInfo && <Splash {...splashInfo} />}
+                            {icon && <Icon icon={icon} size={iconSize} />}
+                        </div>
+                    </Target>
+                );
+            }}
+        </Link>,
+        tooltip
+    );
 };
--- src/components/Button/LinkIconButton/types.ts
+++ src/components/Button/LinkIconButton/types.ts
@@ -1,2 +1,6 @@
-export * from "./LinkIconButton";
-export type * from "./types";
+import type { LinkProps } from "react-aria-components";
+import type { IconButtonBaseProps } from "../core";
+
+export interface LinkIconButtonProps
+    extends Omit<LinkProps, "children" | "className">,
+        IconButtonBaseProps {}
