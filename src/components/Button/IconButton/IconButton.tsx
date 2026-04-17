2c5348dbc2f1f4c2862d4250fb1523e56a08518a
rohan <rohanverma031@gmail.com>
2026-04-17 10:58:12 +0530

    feat: added icon button impl

--- src/components/Button/IconButton/IconButton.tsx
+++ src/components/Button/IconButton/IconButton.tsx
@@ -1,6 +1,21 @@
+import { clsx } from "clsx";
 import { Button as AriaButton } from "react-aria-components";
 import { Icon } from "@/components/Icon";
-import { withTooltip } from "../core";
+import { Loader } from "@/components/Loader";
+import { Splash } from "@/components/Splash/Splash";
+import { useSplash } from "@/components/Splash/useSplash";
+import { Target } from "@/components/Target";
+import {
+    ButtonCore,
+    ButtonIconSizeMapping,
+    ButtonLabelTextMapping,
+    ButtonReset,
+    ButtonSizeBase,
+    buttonColorBase,
+    buttonColorConfig,
+    buttonSizeConfig,
+    withTooltip,
+} from "../core";
 import type { IconButtonProps } from "./types";
 
 export const IconButton: React.FC<IconButtonProps> = ({
@@ -10,11 +25,57 @@ export const IconButton: React.FC<IconButtonProps> = ({
     width = "default",
     tooltip,
     icon,
+    onPress,
     ...props
 }) => {
+    const { splashInfo, handlePress } = useSplash(onPress);
+
     return withTooltip(
-        <AriaButton {...props}>
-            <Icon icon={icon} />
+        <AriaButton
+            onPress={handlePress}
+            className={clsx(ButtonReset)}
+            {...props}
+        >
+            {({
+                isPending,
+                isDisabled,
+                isFocusVisible,
+                isFocused,
+                isPressed,
+                isHovered,
+            }) => {
+                const dataAttrs = {
+                    "data-hovered": isHovered || undefined,
+                    "data-disabled": isDisabled || undefined,
+                    "data-focused": isFocused || undefined,
+                    "data-focus-visible": isFocusVisible || undefined,
+                    "data-pressed": isPressed || undefined,
+                };
+
+                const iconSize = ButtonIconSizeMapping[size];
+                return (
+                    <Target {...dataAttrs}>
+                        <div
+                            className={clsx(
+                                ButtonCore,
+                                buttonColorBase,
+                                ButtonSizeBase,
+                                buttonColorConfig(color),
+                                buttonSizeConfig(size),
+                                ButtonLabelTextMapping[size]
+                            )}
+                            {...dataAttrs}
+                        >
+                            {splashInfo && <Splash {...splashInfo} />}
+                            {isPending ? (
+                                <Loader size={iconSize} />
+                            ) : (
+                                icon && <Icon icon={icon} size={iconSize} />
+                            )}
+                        </div>
+                    </Target>
+                );
+            }}
         </AriaButton>,
         tooltip
     );
--- src/components/Button/IconButton/types.ts
+++ src/components/Button/IconButton/types.ts
@@ -1,5 +1,5 @@
 import type { ButtonProps as AriaButtonProps } from "react-aria-components";
-import type { IconButtonBaseProps } from "../core/iconButton";
+import type { IconButtonBaseProps } from "../core";
 
 export interface IconButtonProps
     extends Omit<AriaButtonProps, "children" | "className">,
