29bac3ffa459c196454b09eba8ae66751b7f37e3
rohan <rohanverma031@gmail.com>
2026-04-17 11:03:06 +0530

    feat: added impl for toggle button impl

--- src/components/Button/ToggleButton/ToggleButton.tsx
+++ src/components/Button/ToggleButton/ToggleButton.tsx
@@ -1,5 +1,20 @@
+import { clsx } from "clsx";
 import { ToggleButton as AriaToggleButton } from "react-aria-components";
-import { withTooltip } from "../core";
+import { Icon } from "@/components/Icon";
+import { Splash } from "@/components/Splash/Splash";
+import { useSplash } from "@/components/Splash/useSplash";
+import { Target } from "@/components/Target";
+import {
+    ButtonCore,
+    ButtonIconSizeMapping,
+    ButtonLabelTextMapping,
+    ButtonReset,
+    ButtonSizeBase,
+    buttonColorBase,
+    buttonColorConfig,
+    buttonSizeConfig,
+    withTooltip,
+} from "../core";
 import type { ToggleButtonProps } from "./types";
 
 export const ToggleButton: React.FC<ToggleButtonProps> = ({
@@ -9,7 +24,68 @@ export const ToggleButton: React.FC<ToggleButtonProps> = ({
     tooltip,
     selectedIcon,
     icon,
+    children,
+    onPress,
     ...props
 }) => {
-    return withTooltip(<AriaToggleButton {...props} />, tooltip);
+    const { splashInfo, handlePress } = useSplash(onPress);
+    const isChildFunc = typeof children === "function";
+
+    return withTooltip(
+        <AriaToggleButton
+            onPress={handlePress}
+            className={clsx(ButtonReset)}
+            {...props}
+        >
+            {(renderProps) => {
+                const {
+                    isSelected,
+                    isDisabled,
+                    isFocusVisible,
+                    isFocused,
+                    isPressed,
+                    isHovered,
+                } = renderProps;
+
+                const dataAttrs = {
+                    "data-hovered": isHovered || undefined,
+                    "data-disabled": isDisabled || undefined,
+                    "data-focused": isFocused || undefined,
+                    "data-focus-visible": isFocusVisible || undefined,
+                    "data-pressed": isPressed || undefined,
+                    "data-selected": isSelected || undefined,
+                    "data-toggle-button": true,
+                };
+
+                let iconToRender = icon;
+                if (isSelected && selectedIcon) iconToRender = selectedIcon;
+
+                const iconSize = ButtonIconSizeMapping[size];
+                return (
+                    <Target {...dataAttrs}>
+                        <div
+                            className={clsx(
+                                ButtonCore,
+                                buttonColorBase,
+                                ButtonSizeBase,
+                                buttonColorConfig(color),
+                                buttonSizeConfig(size),
+                                ButtonLabelTextMapping[size]
+                            )}
+                            {...dataAttrs}
+                        >
+                            {splashInfo && <Splash {...splashInfo} />}
+
+                            {iconToRender && (
+                                <Icon icon={iconToRender} size={iconSize} />
+                            )}
+
+                            {isChildFunc ? children(renderProps) : children}
+                        </div>
+                    </Target>
+                );
+            }}
+        </AriaToggleButton>,
+        tooltip
+    );
 };
