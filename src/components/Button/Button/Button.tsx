0bcc9b278d565a8cb907043b41e211ebc5d527dc
rohan <rohanverma031@gmail.com>
2026-04-17 10:34:35 +0530

    feat: added structure to button

--- src/components/Button/Button/Button.tsx
+++ src/components/Button/Button/Button.tsx
@@ -1,4 +1,9 @@
 import { Button as AriaButton } from "react-aria-components";
+import { Icon } from "@/components/Icon";
+import { Loader } from "@/components/Loader";
+import { Splash } from "@/components/Splash/Splash";
+import { useSplash } from "@/components/Splash/useSplash";
+import { Target } from "@/components/Target";
 import { withTooltip } from "../core";
 import type { ButtonProps } from "./types";
 
@@ -8,7 +13,41 @@ export const Button: React.FC<ButtonProps> = ({
     color = "filled",
     tooltip,
     icon,
+    children,
+    onPress,
     ...props
 }) => {
-    return withTooltip(<AriaButton {...props} />, tooltip);
+    const { splashInfo, handlePress } = useSplash(onPress);
+    const isChildFunc = typeof children === "function";
+
+    return withTooltip(
+        <AriaButton onPress={handlePress} {...props}>
+            {(renderProps) => {
+                const {
+                    isPending,
+                    isDisabled,
+                    isFocusVisible,
+                    isFocused,
+                    isPressed,
+                    isHovered,
+                } = renderProps;
+
+                return (
+                    <Target>
+                        <div>
+                            {splashInfo && <Splash {...splashInfo} />}
+                            {isPending ? (
+                                <Loader />
+                            ) : (
+                                icon && <Icon icon={icon} />
+                            )}
+
+                            {isChildFunc ? children(renderProps) : children}
+                        </div>
+                    </Target>
+                );
+            }}
+        </AriaButton>,
+        tooltip
+    );
 };
