b2c179ce5034b9b8ac341c5fb50e92d23f557ce3
rohan <rohanverma031@gmail.com>
2026-04-18 09:58:29 +0530

    feat: added menu item structure

--- src/components/Menu/MenuItem/MenuItem.tsx
+++ src/components/Menu/MenuItem/MenuItem.tsx
@@ -1,3 +1,37 @@
-export const MenuItem = () => {
-    return <div></div>;
+import { ChevronRight } from "lucide-react";
+import { MenuItem as AriaMenuItem } from "react-aria-components";
+import { Icon } from "@/components/Icon";
+import type { MenuItemProps } from "./types";
+
+export const MenuItem: React.FC<MenuItemProps> = ({
+    leadingIcon,
+    label,
+    supportingText,
+    trailingText,
+    badge,
+    ...props
+}) => {
+    return (
+        <AriaMenuItem {...props}>
+            {({ hasSubmenu }) => {
+                return (
+                    <>
+                        {leadingIcon && <Icon icon={leadingIcon} />}
+
+                        <div>
+                            <p>{label}</p>
+
+                            {supportingText && <p>{supportingText}</p>}
+                        </div>
+
+                        {badge && { badge }}
+
+                        {trailingText && { trailingText }}
+
+                        {hasSubmenu && <Icon icon={ChevronRight} />}
+                    </>
+                );
+            }}
+        </AriaMenuItem>
+    );
 };
---
+++ src/components/Menu/MenuItem/types.ts
@@ -0,0 +1,11 @@
+import type { LucideIcon } from "lucide-react";
+import type { ReactNode } from "react";
+import type { MenuItemProps as AriaMenuItemProps } from "react-aria-components";
+
+export interface MenuItemProps extends Omit<AriaMenuItemProps, "children"> {
+    leadingIcon?: LucideIcon;
+    label: string;
+    supportingText?: string;
+    trailingText?: ReactNode;
+    badge?: ReactNode;
+}
