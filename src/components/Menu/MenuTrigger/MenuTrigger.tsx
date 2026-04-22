34a5f3b74b25b500066b01ed4f71901e215dbb99
Rohan Verma <rohanverma031@gmail.com>
2026-04-22 13:51:04 +0530

    feat: added basic menu trigger impl

--- src/components/Menu/MenuTrigger/MenuTrigger.tsx
+++ src/components/Menu/MenuTrigger/MenuTrigger.tsx
@@ -1,3 +1,20 @@
-export const MenuTrigger = () => {
-    return <div></div>;
+import React from "react";
+import {
+    MenuTrigger as AriaMenuTrigger,
+    type MenuTriggerProps,
+    Popover,
+} from "react-aria-components";
+
+export const MenuTrigger: React.FC<MenuTriggerProps> = ({
+    children,
+    ...props
+}) => {
+    const [trigger, menu] = React.Children.toArray(children);
+
+    return (
+        <AriaMenuTrigger {...props}>
+            {trigger}
+            <Popover>{menu}</Popover>
+        </AriaMenuTrigger>
+    );
 };
