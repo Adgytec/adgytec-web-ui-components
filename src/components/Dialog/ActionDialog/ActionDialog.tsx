4a14d2a83b97fd9b9842a7391c46fac1625ce3c9
rohan <rohanverma031@gmail.com>
2026-04-30 13:41:27 +0530

    feat: added slot title to action dialog heading

--- src/components/Dialog/ActionDialog/ActionDialog.tsx
+++ src/components/Dialog/ActionDialog/ActionDialog.tsx
@@ -29,6 +29,7 @@ export const ActionDialog: React.FC<ActionDialogProps> = ({
 
                             {heading && (
                                 <h2
+                                    slot="title"
                                     className={clsx(
                                         DialogHeadlineTypography,
                                         styles["heading"]
