783dba4dab0cab97f67d61d3a5764d586e4a0744
rohan <rohanverma031@gmail.com>
2026-05-01 12:38:31 +0530

    refactor: removed card comp

--- src/components/Card/GradientCard/GradientCard.tsx
+++
@@ -1,7 +0,0 @@
-// TODO: remove this
-
-import { BaseCard, type StyledBaseCardProps } from "../BaseCard";
-
-export const GradientCard: React.FC<StyledBaseCardProps> = ({ ...props }) => {
-    return <BaseCard {...props} background="gradient" />;
-};
