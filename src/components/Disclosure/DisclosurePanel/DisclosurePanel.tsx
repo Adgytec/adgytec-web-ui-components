10daeb1b1c04f334c46c8236ac22b8881952383a
rohan <rohanverma031@gmail.com>
2026-05-07 14:14:03 +0530

    feat: added styles for disclosure panel

--- src/components/Disclosure/DisclosurePanel/DisclosurePanel.tsx
+++ src/components/Disclosure/DisclosurePanel/DisclosurePanel.tsx
@@ -1,8 +1,28 @@
+import { clsx } from "clsx";
 import {
     DisclosurePanel as AriaDisclosurePanel,
     type DisclosurePanelProps,
 } from "react-aria-components";
+import styles from "./disclosurePanel.module.css";
 
-export const DisclosurePanel: React.FC<DisclosurePanelProps> = (props) => {
-    return <AriaDisclosurePanel {...props} />;
+export const DisclosurePanel: React.FC<DisclosurePanelProps> = ({
+    className,
+    children,
+    ...props
+}) => {
+    return (
+        <AriaDisclosurePanel
+            className={(renderProps) =>
+                clsx(
+                    styles["panel"],
+                    typeof className === "function"
+                        ? className(renderProps)
+                        : className
+                )
+            }
+            {...props}
+        >
+            <div>{children}</div>
+        </AriaDisclosurePanel>
+    );
 };
