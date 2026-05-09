104357cbc0b84d66ca0d1c88e3c4ce974fe8ffa4
rohan <rohanverma031@gmail.com>
2026-05-09 12:56:03 +0530

    Feat: added rich tooltip preview

--- src/components/Tooltip/RichTooltip/RichTooltip/RichTooltip.tsx
+++ src/components/Tooltip/RichTooltip/RichTooltip/RichTooltip.tsx
@@ -1,3 +1,6 @@
+// https://m3.material.io/components/tooltips/guidelines#00e87770-86d0-436d-b50b-436ff3cefe75
+// use this with popover, for more details read the article
+
 import clsx from "clsx";
 import styles from "./richTooltip.module.css";
 
