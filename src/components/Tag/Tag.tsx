a06e028ae643012988d28b00569a9f2a5dd05dc7
rohan <rohanverma031@gmail.com>
2026-05-11 12:55:04 +0530

    feat: added tag component

---
+++ src/components/Tag/Tag.tsx
@@ -0,0 +1,56 @@
+import clsx from "clsx";
+import { X } from "lucide-react";
+import { Tag as AriaTag } from "react-aria-components";
+import { typography } from "@/utils";
+import { IconButton } from "../Button";
+import { Icon } from "../Icon";
+import { TagIconSize } from "./core";
+import styles from "./tag.module.css";
+import type { TagProps } from "./types";
+
+export const Tag: React.FC<TagProps> = ({
+    icon,
+    avatar,
+    label,
+    className,
+    ...props
+}) => {
+    return (
+        <AriaTag
+            className={(renderProps) =>
+                clsx(
+                    styles["tag"],
+                    typeof className === "function"
+                        ? className(renderProps)
+                        : className
+                )
+            }
+            {...props}
+            data-avatar={avatar ? true : undefined}
+            data-icon={!avatar && icon ? true : undefined}
+        >
+            {({ allowsRemoving }) => (
+                <>
+                    {avatar ? (
+                        <span className={clsx(styles["avatar-constraint"])}>
+                            avatar
+                        </span>
+                    ) : (
+                        icon && <Icon icon={icon} size={TagIconSize} />
+                    )}
+
+                    <span className={clsx(typography.labelLarge)}>{label}</span>
+
+                    {allowsRemoving && (
+                        <IconButton
+                            slot="remove"
+                            icon={X}
+                            size="extra-small"
+                            color="standard"
+                        />
+                    )}
+                </>
+            )}
+        </AriaTag>
+    );
+};
