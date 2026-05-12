4d8023566107b9d3eeb7b31c3fb854ffd863feee
rohan <rohanverma031@gmail.com>
2026-05-12 14:05:35 +0530

    feat: added slider variants

--- src/components/Input/Slider/types.ts
+++ src/components/Input/Slider/types.ts
@@ -1,8 +1,21 @@
+import type { LucideIcon } from "lucide-react";
 import type { ReactNode } from "react";
 import type { Slider } from "react-aria-components";
 
+export type SliderVariant = "standard" | "centered" | "range";
+
+export type SliderSize =
+    | "extra-small"
+    | "small"
+    | "medium"
+    | "large"
+    | "extra-large";
+
 export interface SliderProps<T extends number | number[]>
     extends Omit<React.ComponentPropsWithRef<typeof Slider<T>>, "children"> {
+    size?: SliderSize;
+    variant?: SliderVariant;
+    insetIcon?: LucideIcon;
     label?: ReactNode;
     thumbLabels?: string[];
 }
