07905ede184764c1804950eb82dee4a17253a86b
rohan <rohanverma031@gmail.com>
2026-05-15 13:41:16 +0530

    feat: added basic centered slider impl

--- src/components/Input/Slider/CenteredSlider/CenteredSlider.tsx
+++ src/components/Input/Slider/CenteredSlider/CenteredSlider.tsx
@@ -6,6 +6,14 @@ import { SliderThumb } from "../SliderThumb";
 import styles from "./centeredSlider.module.css";
 import type { CenteredSliderProps } from "./types";
 
+function remapBoth(value: number) {
+    const initial = Math.min(1, Math.max(0, value * 2));
+
+    const final = Math.min(1, Math.max(0, (value - 0.5) * 2));
+
+    return { initial, final };
+}
+
 export const CenteredSlider = <T extends number>({
     thumbLabels,
     size = "small",
@@ -25,42 +33,97 @@ export const CenteredSlider = <T extends number>({
             className={clsx(SliderStyles)}
             {...props}
         >
-            {({ orientation, state }) => (
-                <SliderTrack
-                    className={clsx(TrackStyles, SliderSizeStyles(size))}
-                >
-                    <div
-                        className={clsx(styles["visual-track"])}
-                        data-orientation={orientation}
+            {({ orientation, state }) => {
+                const thumbValue = state.getThumbPercent(0);
+
+                const isInitial = thumbValue < 0.5;
+                const isFinal = thumbValue > 0.5;
+                const isMiddle = thumbValue === 0.5;
+
+                const { initial, final } = remapBoth(thumbValue);
+                const initialInactive = initial * 100;
+                const initialActive = (1 - initial) * 100;
+
+                const finalActive = final * 100;
+                const finalInactive = (1 - final) * 100;
+
+                const sizeKey =
+                    orientation === "horizontal" ? "inlineSize" : "blockSize";
+
+                return (
+                    <SliderTrack
+                        className={clsx(TrackStyles, SliderSizeStyles(size))}
                     >
                         <div
-                            className={clsx(styles["active-track"])}
+                            className={clsx(styles["visual-track"])}
                             data-orientation={orientation}
-                            style={{
-                                flexGrow: state.getThumbPercent(0),
-                            }}
-                        />
+                        >
+                            <div
+                                className={clsx(styles["track"])}
+                                data-track="initial"
+                                data-orientation={orientation}
+                                data-active-track={isInitial || undefined}
+                                data-middle={isMiddle || undefined}
+                            >
+                                <div
+                                    className={clsx(styles["inactive-track"])}
+                                    style={{
+                                        [sizeKey]: `${initialInactive}%`,
+                                    }}
+                                />
+                                <div
+                                    className={clsx(styles["active-track"])}
+                                    style={{
+                                        [sizeKey]: `${initialActive}%`,
+                                    }}
+                                    data-orientation={orientation}
+                                    data-track="initial"
+                                    data-active-track={isInitial || undefined}
+                                    data-middle={isMiddle || undefined}
+                                >
+                                    <div />
+                                </div>
+                            </div>
+                            <div
+                                className={clsx(styles["track"])}
+                                data-track="final"
+                                data-orientation={orientation}
+                                data-active-track={isFinal || undefined}
+                                data-middle={isMiddle || undefined}
+                            >
+                                <div
+                                    className={clsx(styles["active-track"])}
+                                    style={{
+                                        [sizeKey]: `${finalActive}%`,
+                                    }}
+                                    data-orientation={orientation}
+                                    data-track="final"
+                                    data-active-track={isFinal || undefined}
+                                    data-middle={isMiddle || undefined}
+                                >
+                                    <div />
+                                </div>
+                                <div
+                                    className={clsx(styles["inactive-track"])}
+                                    style={{
+                                        [sizeKey]: `${finalInactive}%`,
+                                    }}
+                                />
+                            </div>
+                        </div>
+                        <SliderThumb size={size} orientation={orientation} />
 
-                        <div
-                            className={clsx(styles["inactive-track"])}
-                            style={{
-                                flexGrow: 1 - state.getThumbPercent(0),
-                            }}
-                            data-orientation={orientation}
+                        <SliderStops
+                            slider="centered"
+                            minValue={minValue}
+                            maxValue={maxValue}
+                            step={step}
+                            orientation={orientation}
+                            showInBetweenSteps={showInBetweenSteps}
                         />
-                    </div>
-                    <SliderThumb size={size} orientation={orientation} />
-
-                    <SliderStops
-                        slider="centered"
-                        minValue={minValue}
-                        maxValue={maxValue}
-                        step={step}
-                        orientation={orientation}
-                        showInBetweenSteps={showInBetweenSteps}
-                    />
-                </SliderTrack>
-            )}
+                    </SliderTrack>
+                );
+            }}
         </AriaSlider>
     );
 };
