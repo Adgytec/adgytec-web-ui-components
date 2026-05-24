6eb7c716f56159232b14eaa0c5945118d0bdeaec
rohan <rohanverma031@gmail.com>
2026-05-24 17:54:38 +0530

    update: added modal to side sheet

--- src/components/Sheets/SideSheet/SideSheet.tsx
+++ src/components/Sheets/SideSheet/SideSheet.tsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import { X } from "lucide-react";
-import { Dialog, Header, Heading } from "react-aria-components";
+import { Dialog, Header, Heading, Modal } from "react-aria-components";
 import { IconButton } from "@/components/Button";
 import { typography } from "@/utils";
 import styles from "./sideSheet.module.css";
@@ -18,46 +18,49 @@ export const SideSheet: React.FC<SideSheetProps> = ({
     const hasActions = Array.isArray(actions) ? actions.length > 0 : !!actions;
 
     return (
-        <Dialog
-            className={clsx(styles["side-sheet"], className)}
-            {...props}
-            data-alignment={alignment}
-            data-layout={layout}
-            data-actions={hasActions || undefined}
-        >
-            {(renderProps) => (
-                <>
-                    <Header
-                        className={clsx(styles["header"])}
-                        data-headline={!!headline || undefined}
-                    >
-                        {headline && (
-                            <Heading className={clsx(typography.titleLarge)}>
-                                {headline}
-                            </Heading>
-                        )}
-
-                        <IconButton slot="close" icon={X} />
-                    </Header>
-
-                    <div className={clsx(styles["main"])}>
-                        {typeof children === "function"
-                            ? children(renderProps)
-                            : children}
-                    </div>
-
-                    {hasActions && (
-                        <div
-                            className={clsx(styles["actions"])}
-                            data-alignment={alignment}
+        <Modal className={clsx(styles["modal"])} {...props}>
+            <Dialog
+                className={clsx(styles["side-sheet"], className)}
+                data-alignment={alignment}
+                data-layout={layout}
+                data-actions={hasActions || undefined}
+            >
+                {(renderProps) => (
+                    <>
+                        <Header
+                            className={clsx(styles["header"])}
+                            data-headline={!!headline || undefined}
                         >
-                            {typeof actions === "function"
-                                ? actions(renderProps)
-                                : actions}
+                            {headline && (
+                                <Heading
+                                    className={clsx(typography.titleLarge)}
+                                >
+                                    {headline}
+                                </Heading>
+                            )}
+
+                            <IconButton slot="close" icon={X} />
+                        </Header>
+
+                        <div className={clsx(styles["main"])}>
+                            {typeof children === "function"
+                                ? children(renderProps)
+                                : children}
                         </div>
-                    )}
-                </>
-            )}
-        </Dialog>
+
+                        {hasActions && (
+                            <div
+                                className={clsx(styles["actions"])}
+                                data-alignment={alignment}
+                            >
+                                {typeof actions === "function"
+                                    ? actions(renderProps)
+                                    : actions}
+                            </div>
+                        )}
+                    </>
+                )}
+            </Dialog>
+        </Modal>
     );
 };
--- src/components/Sheets/SideSheet/types.ts
+++ src/components/Sheets/SideSheet/types.ts
@@ -1,15 +1,21 @@
 import type { ReactElement, ReactNode } from "react";
-import type { Dialog, DialogRenderProps } from "react-aria-components";
+import type { DialogRenderProps, Modal } from "react-aria-components";
 import type { SheetLayout } from "../core";
 
 export type SideSheetAlignment = "start" | "end";
 
+// children and className are assigned to underlying dialog
 export interface SideSheetProps
-    extends React.ComponentPropsWithRef<typeof Dialog> {
+    extends Omit<
+        React.ComponentPropsWithRef<typeof Modal>,
+        "className" | "children"
+    > {
     headline?: ReactNode;
     alignment?: SideSheetAlignment;
     layout?: SheetLayout;
     actions?:
         | ReactElement[]
         | ((renderProps: DialogRenderProps) => ReactElement[]);
+    children?: ReactNode | ((renderProps: DialogRenderProps) => ReactNode);
+    className?: string;
 }
