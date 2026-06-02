6486db7a5b339e4284d7285c1b3a4335ba2441e5
rohan <rohanverma031@gmail.com>
2026-06-02 14:26:45 +0530

    feat: added impl for nav scroll container

--- src/components/Scaffold/Navigation/NavigationScrollContainer/NavigationScrollContainer.tsx
+++ src/components/Scaffold/Navigation/NavigationScrollContainer/NavigationScrollContainer.tsx
@@ -1,5 +1,32 @@
+import { useLayoutEffect } from "react";
+import { useObjectRef } from "react-aria";
+import { useNavigationInfo } from "../core";
+import { useNavigationState } from "../NavigationState";
+
 export const NavigationScrollContainer: React.FC<
     React.ComponentPropsWithRef<"div">
-> = (props) => {
-    return <div {...props} />;
+> = ({ ref, ...props }) => {
+    const scrollContainerRef = useObjectRef(ref);
+
+    const { saveNavigationScrollTop, getNavigationScrollTop } =
+        useNavigationState();
+    const { id } = useNavigationInfo();
+
+    useLayoutEffect(() => {
+        const scrollTop = getNavigationScrollTop(id);
+
+        if (scrollContainerRef.current) {
+            scrollContainerRef.current.scrollTop = scrollTop;
+        }
+    }, [scrollContainerRef, id, getNavigationScrollTop]);
+
+    return (
+        <div
+            ref={scrollContainerRef}
+            onScroll={(e) => {
+                saveNavigationScrollTop(id, e.currentTarget.scrollTop);
+            }}
+            {...props}
+        />
+    );
 };
