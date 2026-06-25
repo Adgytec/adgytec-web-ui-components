43df4310f4b9dd1a3b9897240dce8442cd8a02cc
rohan <rohanverma031@gmail.com>
2026-06-25 15:29:47 +0530

    update: removed post css custom media queries

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,14 +1,3 @@
-const postcssGlobalData = require("@csstools/postcss-global-data");
-
 module.exports = {
-    plugins: [
-        postcssGlobalData({
-            files: ["./src/styles/core/tokens/layout.css"],
-        }),
-        require("postcss-nesting"),
-        require("postcss-custom-media")({
-            preserve: true,
-        }),
-        require("autoprefixer"),
-    ],
+    plugins: [require("postcss-nesting"), require("autoprefixer")],
 };
